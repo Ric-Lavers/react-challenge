11cb3cdde1176896b03d23b9ce485a207120da48
Patrick Smith <patrick@burntcaramel.com>
2017-11-29 11:13:02 +1100

    Add state to app, and use it in render()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: {
+      firstName: 'Alice',
+      lastName: 'Jones',
+      profileImageURL: 'https://randomuser.me/api/portraits/women/50.jpg'
+    }
+  }
+
   render() {
+    const user = this.state.user
+
     return (
       <div className="App">
         <h1>LinkedIn Profile Editor</h1>
-        <img src='https://randomuser.me/api/portraits/women/50.jpg' />
-        <p>Name: Alice Jones</p>
+        <img src={ user.profileImageURL } />
+        <p>Name: { user.firstName } { user.lastName }</p>
       </div>
     );
   }
